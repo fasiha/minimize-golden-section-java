80ba73d098e1270926ab23feb57e0db4461100f7
Ahmed Fasih <wuzzyview@gmail.com>
2019-11-02 22:10:19 -0400

    init

---
+++ src/main/java/me/aldebrn/mingolden/App.java
@@ -0,0 +1,13 @@
+package me.aldebrn.mingolden;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
